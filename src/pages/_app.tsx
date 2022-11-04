9473495ce9a034a471c8a90319157e3ee46f9861
AmoabaKelvin <kel.amoaba@gmail.com>
2022-11-04 09:51:08 +0000

    fix: remove seo from index to _app file

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,10 +1,12 @@
 import "../../styles/globals.css";
 import type { AppProps } from "next/app";
 import { ThemeProvider } from "next-themes";
+import Seo from "../seo/Seo";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <ThemeProvider attribute="class" enableSystem={false}>
+      <Seo />
       <Component {...pageProps} />
     </ThemeProvider>
   );
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,14 +2,12 @@ import Layout from "../components/Layout";
 import Introduction from "../components/UI/Introduction";
 import Skills from "../components/UI/Skills";
 import Projects from "../components/UI/Projects";
-import Head from "next/head";
 import Contact from "../components/UI/Contact";
-import Seo from "../seo/Seo";
 
 export default function Home() {
   return (
     <Layout>
-      <Seo />
+      {/* <Seo /> */}
       <Introduction />
       <Skills />
       <Projects />
