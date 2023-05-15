339af99845c031a36670555a9c69e736c1052075
Kelvin Amoaba <97001695+AmoabaKelvin@users.noreply.github.com>
2023-05-15 01:21:21 +0000

    feat: add new projects and change projects order

--- data/portfolio.ts
+++ data/portfolio.ts
@@ -1,5 +1,59 @@
 export const PORTFOLIO_ITEMS = [
   {
+    id: 8,
+    image: "/images/portfolio/telegram.webp",
+    title: "Ultra URL Bot",
+    description:
+      "A Telegram bot for URL shortening, expanding and QRCode generation powered by MongoDB",
+    link: "https://t.me/q_r_code_bot",
+    github: "",
+  },
+  {
+    id: 9,
+    image: "/images/portfolio/bigquery.webp",
+    title: "Scrapy BigQuery Pipeline",
+    description:
+      "Contributed to the development of an open-source bigquery pipeline for Scrapy framework",
+    link: "",
+    github: "https://github.com/8W9aG/scrapy-bigquery",
+  },
+  {
+    id: 10,
+    image: "/images/portfolio/bazaar.png",
+    title: "Bazaar",
+    description:
+      "Developed an automated web scraping tool, capable of developing web scrapers for any website in a few clicks, including deployment to the cloud",
+    link: "",
+    github: "",
+  },
+  {
+    id: 11,
+    image: "/images/portfolio/white.png",
+    title: "Edugrids",
+    description:
+      "Led the development of an online forum for students and teachers to share ideas and resources, with over 3000 active users",
+    link: "",
+    github: "",
+  },
+  {
+    id: 7,
+    image: "/images/portfolio/api.png",
+    title: "Vinyl",
+    description:
+      "An API for a music store built with Django Rest Framework, PostgreSQL and integrated with Stripe",
+    link: "",
+    github: "https://github.com/AmoabaKelvin/vinyl-backend",
+  },
+  {
+    id: 3,
+    image: "/images/portfolio/kairos.png",
+    title: "Kairos",
+    description:
+      "A laboratory management system built with Python, Django, PostgreSQL and Bootstrap",
+    link: "",
+    github: "https://github.com/AmoabaKelvin/kairos-medical-center",
+  },
+  {
     id: 1,
     image: "/images/portfolio/hulu-clone.png",
     title: "Hulu Website Clone",
@@ -15,15 +69,6 @@ export const PORTFOLIO_ITEMS = [
     description: "Banking website built with HTML,CSS and JS",
     link: "https://amoaba-bankist.netlify.app/",
     github: "https://github.com/AmoabaKelvin/bankist",
-  },
-  {
-    id: 3,
-    image: "/images/portfolio/kairos.png",
-    title: "Kairos",
-    description:
-      "A laboratory management system built with Python, Django, PostgreSQL and Bootstrap",
-    link: "",
-    github: "https://github.com/AmoabaKelvin/kairos-medical-center",
   },
   {
     id: 4,
@@ -48,23 +93,5 @@ export const PORTFOLIO_ITEMS = [
     description: "A blogging website built with Django, HTML, CSS and JS",
     link: "",
     github: "https://github.com/AmoabaKelvin/kelvins-insights",
-  },
-  {
-    id: 7,
-    image: "/images/portfolio/api.png",
-    title: "Vinyl",
-    description:
-      "An API for a music store built with Django Rest Framework, PostgreSQL and integrated with Stripe",
-    link: "",
-    github: "https://github.com/AmoabaKelvin/vinyl-backend",
-  },
-  {
-    id: 8,
-    image: "/images/portfolio/telegram.webp",
-    title: "Ultra URL Bot",
-    description:
-      "A Telegram bot for URL shortening, expanding and QRCode generation powered by MongoDB",
-    link: "https://t.me/q_r_code_bot",
-    github: "",
   },
 ];
