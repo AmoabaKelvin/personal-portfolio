8c444421f31c1b6c1bd0fc36a4ef7124fca0a925
Kelvin Amoaba <97001695+AmoabaKelvin@users.noreply.github.com>
2023-05-15 01:21:58 +0000

    feat: add animation to hover actions

--- src/components/links/Link.tsx
+++ src/components/links/Link.tsx
@@ -10,7 +10,7 @@ const Link = ({ href, className, children }: LinkProps) => {
   return (
     <a
       href={href}
-      className={`inline-block py-3 px-4 dark:bg-primary bg-accent text-white rounded dark:hover:bg-secondary hover:bg-secondary ${className}`}
+      className={`inline-block py-3 px-4 dark:bg-primary bg-accent text-white rounded dark:hover:bg-secondary hover:bg-secondary ${className} duration`}
     >
       {children}
     </a>
