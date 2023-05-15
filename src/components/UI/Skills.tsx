d9629e7ae8f98d67e542c4a13857cafd5d139373
Kelvin Amoaba <97001695+AmoabaKelvin@users.noreply.github.com>
2023-05-15 01:17:24 +0000

    feat: update skills

--- src/components/UI/Skills.tsx
+++ src/components/UI/Skills.tsx
@@ -1,109 +1,98 @@
-import React from "react";
-import Link from "../links/Link";
 import {
-  SiJavascript,
-  SiTypescript,
-  SiReact,
-  SiTailwindcss,
-  SiPostgresql,
-  SiMongodb,
-  SiGithub,
-  SiGit,
-  SiHeroku,
-  SiNetlify,
-  SiFigma,
-  SiAdobeillustrator,
-  SiAdobephotoshop,
-  SiAdobexd,
-  SiNextdotjs,
-  SiPython,
+  SiAmazonaws,
   SiDjango,
-  SiHtml5,
-  SiCss3,
+  SiGit,
+  SiGithub,
+  SiJavascript,
   SiLinux,
-  SiBootstrap,
-  SiSelenium,
+  SiMongodb,
+  SiNextdotjs,
+  SiPostgresql,
+  SiPython,
+  SiReact,
+  SiRedis,
+  SiTypescript,
 } from "react-icons/si";
 
-import { DiPostgresql } from "react-icons/di";
+import { FaJava, FaNodeJs } from "react-icons/fa";
+import { TbBrandGoogleBigQuery } from "react-icons/tb";
 
+const skills = [
+  {
+    name: "JavaScript",
+    icon: <SiJavascript />,
+  },
+  {
+    name: "TypeScript",
+    icon: <SiTypescript />,
+  },
+  {
+    name: "Java",
+    icon: <FaJava />,
+  },
+  {
+    name: "Python",
+    icon: <SiPython />,
+  },
+  {
+    name: "PostgreSQL",
+    icon: <SiPostgresql />,
+  },
+  {
+    name: "MongoDB",
+    icon: <SiMongodb />,
+  },
+  {
+    name: "Google BigQuery",
+    icon: <TbBrandGoogleBigQuery />,
+  },
+  {
+    name: "Redis",
+    icon: <SiRedis />,
+  },
+  {
+    name: "React JS",
+    icon: <SiReact />,
+  },
+  {
+    name: "Next JS",
+    icon: <SiNextdotjs />,
+  },
+  {
+    name: "Django",
+    icon: <SiDjango />,
+  },
+  {
+    name: "Node JS",
+    icon: <FaNodeJs />,
+  },
+  {
+    name: "Linux",
+    icon: <SiLinux />,
+  },
+  {
+    name: "Git",
+    icon: <SiGit />,
+  },
+  {
+    name: "GitHub",
+    icon: <SiGithub />,
+  },
+  {
+    name: "AWS",
+    icon: <SiAmazonaws />,
+  },
+];
 const Skills = () => {
-  const skills = [
-    {
-      name: "JavaScript",
-      icon: <SiJavascript />,
-    },
-    {
-      name: "TypeScript",
-      icon: <SiTypescript />,
-    },
-    {
-      name: "React",
-      icon: <SiReact />,
-    },
-    {
-      name: "Next JS",
-      icon: <SiNextdotjs />,
-    },
-    {
-      name: "Python",
-      icon: <SiPython />,
-    },
-    {
-      name: "Django",
-      icon: <SiDjango />,
-    },
-    {
-      name: "Tailwind",
-      icon: <SiTailwindcss />,
-    },
-    {
-      name: "PostgreSQL",
-      icon: <SiPostgresql />,
-    },
-    {
-      name: "HTML",
-      icon: <SiHtml5 />,
-    },
-    {
-      name: "CSS",
-      icon: <SiCss3 />,
-    },
-    {
-      name: "MongoDB",
-      icon: <SiMongodb />,
-    },
-    {
-      name: "Linux",
-      icon: <SiLinux />,
-    },
-    {
-      name: "Git",
-      icon: <SiGit />,
-    },
-    {
-      name: "GitHub",
-      icon: <SiGithub />,
-    },
-    {
-      name: "Bootstrap",
-      icon: <SiBootstrap />,
-    },
-    {
-      name: "Selenium",
-      icon: <SiSelenium />,
-    },
-  ];
-
   return (
     <div className="dark:text-slate-300 text-slate-800 mt-8">
       <h1 className="text-3xl font-bold mb-4">Technical Skills</h1>
       <div className="grid grid-cols-2 gap-4 sm:grid-cols-3 md:grid-cols-4">
         {skills.map((skill, index) => (
-          <Link href="#" key={index} className="flex items-center gap-1">
+          <span key={index} className="flex items-center gap-1 px-2 py-1">
             {skill.icon}
             {skill.name}
-          </Link>
+          </span>
         ))}
       </div>
     </div>
