b80fa9a236138c5d70af94496ac880c04244bba3
Kelvin Amoaba <97001695+AmoabaKelvin@users.noreply.github.com>
2023-05-15 01:20:05 +0000

    feat: update links in meta tags

--- src/seo/Seo.tsx
+++ src/seo/Seo.tsx
@@ -1,4 +1,3 @@
-import { NextSeo } from "next-seo";
 import Head from "next/head";
 
 const Seo = () => (
@@ -10,20 +9,20 @@ const Seo = () => (
       <link rel="icon" href="/favicon/favicon.ico" />
       <meta
         name="description"
-        content="Full-stack Software Engineer with passion for building beautiful and fully functional web applications."
+        content="Software Engineer with passion for building beautiful and fully functional web applications."
       />
 
       <meta property="og:type" content="website" />
-      <meta property="og:url" content="https://www.kelvinamoaba.me/" />
+      <meta property="og:url" content="https://www.kelvinamoaba.live/" />
       <meta property="og:title" content="Kelvin Amoaba - Portfolio" />
       <meta
         property="og:description"
-        content="Full-stack Software Engineer with passion for building beautiful and fully functional web applications."
+        content="Software Engineer with passion for building beautiful and fully functional web applications."
       />
       <meta property="og:image" content="/images/portfolio/portfolio.png" />
 
       <meta property="twitter:card" content="summary" />
-      <meta property="twitter:url" content="https://www.kelvinamoaba.me/" />
+      <meta property="twitter:url" content="https://www.kelvinamoaba.live/" />
       <meta property="twitter:title" content="Kelvin Amoaba - Portfolio" />
       <meta
         property="twitter:description"
